36132a3352fd0c6823b24f41d617961d2ac2af00
Jagadeep Mamidi <jagadeep.mamidi@gmail.com>
2024-01-08 00:22:25 +0530

    first commit

---
+++ assets/backgroundimage/img/index.js
@@ -0,0 +1,7 @@
+
+import haze from '../img/haze.jpg';
+import rainy from '../img/rainy.jpg';
+import snow from '../img/snow.jpg';
+import sunny from '../img/sunny.jpg';
+
+export { haze, rainy, snow, sunny };
